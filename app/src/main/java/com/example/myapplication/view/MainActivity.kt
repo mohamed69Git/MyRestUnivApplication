d9c64a6744e31d37c42205a56dae035b13813b40
mohamed <mohamedgithub69@gmail.com>
2022-05-01 03:57:15 +0000

    changing logo and checking connectivity

--- app/src/main/java/com/example/myapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/view/MainActivity.kt
@@ -4,7 +4,8 @@ import android.app.ActivityOptions
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.os.Handler
 import android.util.Pair
@@ -14,6 +15,8 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 
 
@@ -35,9 +38,9 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         //removing the actionBar
-        window.setFlags(    FLAG_FULLSCREEN, FLAG_FULLSCREEN)
+        window.setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main);
-//        textView.text = "Bonjour"
+        //textView.text = "Bonjour"
         //Animations
         topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
         bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
@@ -50,15 +53,23 @@ class MainActivity : AppCompatActivity() {
         image.animation = topAnimation
         logo.animation = bottomAnimation
         slogan.animation = bottomAnimation
-
+        //checking connectivity
+        val ConnectionManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = ConnectionManager.activeNetworkInfo
+        if (networkInfo != null && networkInfo.isConnected == true) {
+            Toast.makeText(this@MainActivity, "Network Available", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this@MainActivity, "Network Not Available", Toast.LENGTH_LONG).show()
+        }
 
 
         //creating a new intent
         Handler().postDelayed(fun() {
 
             val intent = Intent(this, Login::class.java)
-            var a: View  = logo
-            var b: String  = "logo_text"
+            var a: View = logo
+            var b: String = "logo_text"
             var c: View = image
             var d: String = "logo_image"
             var pair1 = Pair(a, b)
@@ -66,15 +77,16 @@ class MainActivity : AppCompatActivity() {
 
 
             val options = ActivityOptions.makeSceneTransitionAnimation(this, pair1, pair2)
-            val sharedPreferences1: SharedPreferences = getSharedPreferences("user_information", Context.MODE_PRIVATE)
+            val sharedPreferences1: SharedPreferences =
+                getSharedPreferences("user_information", Context.MODE_PRIVATE)
             val token = sharedPreferences1.getString("token", null)
-            if(token !="" && token != null){
+            if (token != "" && token != null) {
                 startActivity(Intent(this, Dashboard::class.java))
                 finish()
-            }
-            else{
+            } else {
                 startActivity(intent, options.toBundle())
                 finish()
+
             }
         }, SPLASH_SCREEN)
 
